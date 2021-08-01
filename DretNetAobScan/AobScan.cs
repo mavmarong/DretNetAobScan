b1c77d39f57afae12ab17f4b94561beb8d378deb
Marco M <marongiumarco4@gmail.com>
2021-08-01 17:07:30 +0200

    new pattern scan system & code cleaning

--- DretNetAobScan/AobScan.cs
+++ DretNetAobScan/AobScan.cs
@@ -13,10 +13,8 @@ namespace DretNetAobScan
         #region Pinvokes
         [DllImport("ntdll.dll", SetLastError = true)]
         public static extern bool NtReadVirtualMemory(IntPtr ProcessHandle, IntPtr BaseAddress, [Out] byte[] Buffer, UInt32 NumberOfBytesToRead, IntPtr NumberOfBytesRead);
-
         [DllImport("kernel32.dll", SetLastError = true)]
         public static extern IntPtr OpenProcess(Int32 processAccess, bool bInheritHandle, int processId);
-
         [DllImport("ntdll.dll", SetLastError = true)]
         public static extern bool NtWriteVirtualMemory(IntPtr ProcessHandle, IntPtr BaseAddress, byte[] Buffer, UInt32 NumberOfBytesToWrite, IntPtr NumberOfBytesWritten);
         #endregion
@@ -42,64 +40,33 @@ namespace DretNetAobScan
             byte[] __read_buffer = new byte[8192];
 
             for (long i = 0; i < 0x7FFFFF; i += 8192) {
-
                 uint __buffer_value = (uint)__read_buffer.Length;
-
                 NtReadVirtualMemory(process().Handle, new IntPtr(i), __read_buffer, __buffer_value, IntPtr.Zero);
-
-                int offset = __pattern_scan(__read_buffer, __pattern);
-
-                if (offset != 1) {
-
+                long offset = __pattern_scan(__read_buffer, __pattern);
+                if (offset != 1)
                     __addresses.Add(new IntPtr(i + offset));
-
-                }
             }
         }
 
         public void __write_memory() {
             for (int i = 0; i < __addresses.Count(); i++) {
                 uint __buffer_value = (uint)__buffer.Length;
-
                 NtWriteVirtualMemory(process().Handle, __addresses[i], __buffer, __buffer_value, IntPtr.Zero);
             }
         }
 
-        private int __pattern_scan(byte[] buffer, byte[] pattern) {
-
-            for (int i = 0; i < buffer.Length; i++) {
-
-                for (int j = 0; pattern[j] == buffer[i]; j++) {
-
-                    for (int k = 1; pattern[j + k] == buffer[i + k]; k++) {
-
-                        int x = j + k;
-                        int y = i + k;
-
-                        while (true) {
-
-                            if (pattern[x] == pattern[y]) {
-
-                                x++; y++;
-
-                                if (x == pattern.Length || y == buffer.Length) {
-
-                                    return i;
-
-                                }
-
-                            } else {
-
-                                break;
-
-                            }
-
-                        }
-
+        private long __pattern_scan(byte[] buffer, byte[] pattern) {
+            long i = 0; long j = 0; long l = 0;
+            while (i != buffer.Length) {
+                if (buffer[i] == pattern[0]) {
+                    j = i;
+                    l = 0;
+                    while (buffer[j] == pattern[l]) {
+                        j++; l++;
+                        if (j == buffer.Length || l == pattern.Length) return i;
                     }
-
                 }
-
+                i++;
             }
             return -1;
         }
