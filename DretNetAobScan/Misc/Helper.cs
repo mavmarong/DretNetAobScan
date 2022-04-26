5dd517eab62b5f0073fcdf0569d5cc624ba393ee
Marco Marongiu <marongiumarco4@gmail.com>
2022-04-26 20:13:55 +0200

    removed unused usings.

--- DretNetAobScan/Misc/Helper.cs
+++ DretNetAobScan/Misc/Helper.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace DretNetAobScan {
     public class Helper {
